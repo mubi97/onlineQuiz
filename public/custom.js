f0ae3bd3c1d116f308c01e3fc50b526c3105c016
mubi97 <p156172@nu.edu.pk>
2020-08-13 22:44:36 +0500

    cookies Added

--- public/custom.js
+++ public/custom.js
@@ -1,30 +1,62 @@
 $(document).ready(function () {
     var ll = 0;
+    function setCookie(key, value, expiry) {
+        var expires = new Date();
+        expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+        document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+    }
+
+    function getCookie(key) {
+        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+        return keyValue ? keyValue[2] : null;
+    }
     $(".questions").click(function (e) {
         var row = $(this);
-        var data = {
-            questionId: $(this).find(".id").html()
-        };
-        $.ajax({
-            type: "POST",
-            url: 'loadAnswers.php',
-            data: data,
-            success: function (response) {
-                // var src = '<div class="form-group"><label for="sel1">Select Answer:</label><select class="form-control">';
-                var select = $("<select class="+ll+"></select>");
-                ll++;
-                response = JSON.parse(response);
-                $.each(response, function (index, value) {
-                    // src += '<option value="' + value.id + '">' + value.answer + '</option>';
-                    var opt = $("<option></option");
-                    opt.val(value.id);
-                    opt.html(value.answer);
-                    select.append(opt);
-                    select.val(opt.val());
-                });
-                row.find(".answerBox").html(select);
-            }
-        });
+        if(row.find(".answerBox").html() == '') {
+            var data = {
+                questionId: $(this).find(".id").html()
+            };
+            $.ajax({
+                type: "POST",
+                url: 'loadAnswers.php',
+                data: data,
+                success: function (response) {
+                    var src = '<div class="form-group"><label for="sel1">Select Answer:</label><select class="form-control">';
+                    var select = $("<select class="+ll+"></select>");
+                    ll++;
+                    response = JSON.parse(response);
+    
+                    $.each(response, function (index, value) {
+                        // src += '<option value="' + value.id + '">' + value.answer + '</option>';
+                        var opt = $("<option></option");
+                        opt.val(value.id);
+                        opt.html(value.answer);
+                        select.append(opt);
+                        select.val(opt.val());
+                    });
+                   
+                   
+                    row.find(".answerBox").html(select);
+                    var questionId = row.find("td").eq(0).text();
+                    if(getCookie('question'+questionId) != '') {
+
+                        var answerId = getCookie('question'+questionId);
+                        row.find(".answerBox").find('select').val(answerId);
+                    } else {
+                        var answerId = row.find(".answerBox").find(':selected').val();
+
+                        setCookie('question'+questionId, answerId, 10);
+                    }
+                   
+                }
+            });
+        } else {
+            var questionId = row.find("td").eq(0).text();
+            var answerId = row.find(".answerBox").find(':selected').val();
+
+            setCookie('question'+questionId, answerId, 10);
+
+        }
     });
     $("#submit-btn").click(function(e) {
         var table = $(".table tbody");
